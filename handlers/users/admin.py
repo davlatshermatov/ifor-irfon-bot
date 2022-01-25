da19f82130bb1f72c7e490ccfa835644f2c3bd70
Davlat Shermatov <davlatshermatov111@gmail.com>
2022-01-25 08:41:53 +0500

    admin handdler updated

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -9,7 +9,7 @@ async def get_file_id(message: types.Message):
     await message.reply("File ID: " + message.photo[-1].file_id)  # file_id
 
 
-# only for admins
-@dp.message_handler(content_types=types.ContentType.DOCUMENT, user_id=ADMINS)
-async def get_file_id(message: types.Message):
-    await message.reply("File ID: " + message.document.file_id)  # file_id
+# # only for admins
+# @dp.message_handler(content_types=types.ContentType.DOCUMENT, user_id=ADMINS)
+# async def get_file_id(message: types.Message):
+#     await message.reply("File ID: " + message.document.file_id)  # file_id
